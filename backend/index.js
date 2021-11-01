9bc2b12b9fc69c3381c377e4b6e39631b171566b
JolaWalach <joanna.walach@op.pl>
2021-11-01 19:40:58 +0100

    frontend folder created

--- backend/index.js
+++ backend/index.js
@@ -30,7 +30,7 @@ router.route("/getData").get(function(req, res) {
         res.send(result)
     }
 });
-}).pretty();
+});
 
 app.listen(PORT, function() {
     console.log("Server is running on port: "+ PORT);
--- backend/model.js
+++ backend/model.js
@@ -3,7 +3,9 @@ const Schema =mongoose.Schema;
 
 let user = new Schema({
     name: { type: String}, 
-    surname: { type: String}
+    surname: { type: String}, 
+    age: {type: Number},
+    position: {type:String}
 });
 
 module.exports = mongoose.model("user", user);
